604c1487e62287ade92de566052f74c898d2d53f
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-12 01:11:11 +0700

    create pending user model and migrate

--- accounts/models.py
+++ accounts/models.py
@@ -14,3 +14,18 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
+
+class PendingUser(BaseModel):
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    verification_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self) -> bool:
+        lifespan_in_seconds = 20 * 60
+        now = datetime.now(timezone.utc)
+        timediff = now - self.created_at
+        timediff = timediff.total_seconds()
+        if timediff > lifespan_in_seconds:
+            return False
+        return True
