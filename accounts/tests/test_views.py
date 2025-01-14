45c0437b029c06152b71aa7aa35e42cbe51d0380
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-15 01:11:11 +0700

    create register and verify account testing

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.hashers import check_password
 from django.contrib.messages import get_messages
 from django.test.client import Client
 from django.urls import reverse
-from accounts.models import PendingUser
+from django.contrib.auth import get_user
+from accounts.models import PendingUser, User
 
 pytestmark = pytest.mark.django_db
 
@@ -21,3 +22,41 @@ def test_register_user(client: Client):
     assert len(messages) == 1
     assert messages[0].level_tag == "success"
     assert "Verification code sent to" in str(messages[0])
+
+def test_register_user_duplicate_email(client: Client, user_instance):
+    url = reverse("register")
+    request_data = {"email": user_instance.email, "password": "abc"}
+    response = client.post(url, request_data)
+    assert response.status_code == 302
+    assert response.url == reverse("register")
+
+    messages = list(get_messages(response.wsgi_request))
+    assert len(messages) == 1
+    assert messages[0].level_tag == "error"
+    assert "Email exists on the platform" in str(messages[0])
+
+def test_verify_account_valid_code(client: Client):
+    pending_user = PendingUser.objects.create(
+        email="abc@gmail.com", verification_code="5555", password="randompass"
+    )
+    url = reverse("verify_account")
+    request_data = {"email": pending_user.email, "code": pending_user.verification_code}
+    response = client.post(url, request_data)
+    assert response.status_code == 302
+    assert response.url == reverse("home")
+    user = get_user(response.wsgi_request)
+    assert user.is_authenticated
+
+def test_verify_account_invalid_code(client: Client):
+    pending_user = PendingUser.objects.create(
+        email="abc@gmail.com", verification_code="5555", password="randompass"
+    )
+    url = reverse("verify_account")
+    request_data = {"email": pending_user.email, "code": "invalidcode"}
+    response = client.post(url, request_data)
+    assert response.status_code == 400
+    assert User.objects.count() == 0
+
+    messages = list(get_messages(response.wsgi_request))
+    assert len(messages) == 1
+    assert messages[0].level_tag == "error"
--- conftest.py
+++ conftest.py
@@ -1,6 +1,15 @@
 import pytest
+
+from django.contrib.auth.hashers import make_password
 from django.test.client import Client
+from accounts.models import User
 
 @pytest.fixture
 def client():
     return Client()
+
+@pytest.fixture
+def user_instance(db):
+    return User.objects.create(
+        email="randomabc@gmail.com", password=make_password("abcd")
+    )
