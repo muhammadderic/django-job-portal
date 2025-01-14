ea3b7c27e65a378c9565734016673638d097dfa3
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-15 01:11:11 +0700

    create token model and token type, migrate the new model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from datetime import datetime, timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
@@ -5,6 +7,9 @@ from django.db import models
 from common.models import BaseModel
 
 from .manager import CustomUserManager
+
+class TokenType(models.TextChoices):
+    PASSWORD_RESET = ("PASSWORD_RESET", "PASSWORD_RESET")
 
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
@@ -24,6 +29,25 @@ class PendingUser(BaseModel):
 
     def is_valid(self) -> bool:
         lifespan_in_seconds = 20 * 60
+        now = datetime.now(timezone.utc)
+        timediff = now - self.created_at
+        timediff = timediff.total_seconds()
+        if timediff > lifespan_in_seconds:
+            return False
+        return True
+
+class Token(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+    token_type = models.CharField(max_length=100, choices=TokenType.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}  {self.token}"
+
+    def is_valid(self) -> bool:
+        lifespan_in_seconds = 20 * 60  # 20 mins
         now = datetime.now(timezone.utc)
         timediff = now - self.created_at
         timediff = timediff.total_seconds()
