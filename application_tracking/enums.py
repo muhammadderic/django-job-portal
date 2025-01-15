68876f6dc151c76302d0dccf3c77048fb9ad6a30
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-16 01:11:11 +0700

    create enums and JobAdvert model

---
+++ application_tracking/enums.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class EmploymentType(models.TextChoices):
+    FULL_TIME = "Full Time", "Full Time"
+    PART_TIME = "Part Time", "Part Time"
+    CONTRACT = "Contract", "Contract"
+
+
+class ExperienceLevel(models.TextChoices):
+    ENTRY_LEVEL = "Entry Level", "Entry Level"
+    MID_LEVEL = "Mid Level", "Mid Level"
+    SENIOR = "Senior", "Senior"
+
+
+class LocationType(models.TextChoices):
+    ONSITE = "Onsite", "Onsite"
+    HYBRID = "Hybrid", "Hybrid"
+    REMOTE = "Remote", "Remote"
--- application_tracking/models.py
+++ application_tracking/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from common.models import BaseModel
+
+from .enums import (EmploymentType, ExperienceLevel, LocationType)
+
+class JobAdvert(BaseModel):
+    title = models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150)
+    employment_type = models.CharField(
+        max_length=50,
+        choices=EmploymentType.choices,
+        default=EmploymentType.FULL_TIME,
+    )
+    experience_level = models.CharField(
+        max_length=50,
+        choices=ExperienceLevel.choices,
+        default=ExperienceLevel.ENTRY_LEVEL,
+    )
+    description = models.TextField()
+    job_type = models.CharField(
+        max_length=50,
+        choices=LocationType.choices,
+        default=LocationType.ONSITE,
+    )
+    location =  models.CharField(max_length=255, null=True, blank=True)
+    is_published = models.BooleanField(default=True)
+    deadline = models.DateField()
+    skills = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-created_at",)
+    
+    def publish_advert(self) -> None:
+        self.is_published = True
+        self.save(update_fields=["is_published"])
